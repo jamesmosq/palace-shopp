db6101049a508603c1c028ea0005af4d502ba7a9
James Mosquera <Jamesmosqr@gmail.com>
2025-11-25 00:39:25 -0500

    first commit

---
+++ app/MoonShine/Resources/Product/Pages/ProductDetailPage.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\MoonShine\Resources\Product\Pages;
+
+use MoonShine\Laravel\Pages\Crud\DetailPage;
+use MoonShine\Contracts\UI\ComponentContract;
+use MoonShine\UI\Components\Table\TableBuilder;
+use MoonShine\Contracts\UI\FieldContract;
+use App\MoonShine\Resources\Product\ProductResource;
+use MoonShine\Support\ListOf;
+use MoonShine\UI\Fields\ID;
+use MoonShine\UI\Fields\Text;
+use MoonShine\UI\Fields\Number;
+use Throwable;
+
+
+/**
+ * @extends DetailPage<ProductResource>
+ */
+class ProductDetailPage extends DetailPage
+{
+    /**
+     * @return list<FieldContract>
+     */
+    protected function fields(): iterable
+    {
+        return [
+            ID::make(),
+            Text::make('Nombre', 'name')->sortable(),
+            Text::make('Descripción', 'description'),
+            Number::make('Precio', 'price')->sortable(),
+            Number::make('Stock', 'stock')->sortable(),
+
+        ];
+    }
+
+    protected function buttons(): ListOf
+    {
+        return parent::buttons();
+    }
+
+    /**
+     * @param  TableBuilder  $component
+     *
+     * @return TableBuilder
+     */
+    protected function modifyDetailComponent(ComponentContract $component): ComponentContract
+    {
+        return $component;
+    }
+
+    /**
+     * @return list<ComponentContract>
+     * @throws Throwable
+     */
+    protected function topLayer(): array
+    {
+        return [
+            ...parent::topLayer()
+        ];
+    }
+
+    /**
+     * @return list<ComponentContract>
+     * @throws Throwable
+     */
+    protected function mainLayer(): array
+    {
+        return [
+            ...parent::mainLayer()
+        ];
+    }
+
+    /**
+     * @return list<ComponentContract>
+     * @throws Throwable
+     */
+    protected function bottomLayer(): array
+    {
+        return [
+            ...parent::bottomLayer()
+        ];
+    }
+}
---
+++ app/MoonShine/Resources/Product/ProductResource.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\MoonShine\Resources\Product;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Product;
+use App\MoonShine\Resources\Product\Pages\ProductIndexPage;
+use App\MoonShine\Resources\Product\Pages\ProductFormPage;
+use App\MoonShine\Resources\Product\Pages\ProductDetailPage;
+
+use MoonShine\Laravel\Resources\ModelResource;
+use MoonShine\Contracts\Core\PageContract;
+
+/**
+ * @extends ModelResource<Product, ProductIndexPage, ProductFormPage, ProductDetailPage>
+ */
+class ProductResource extends ModelResource
+{
+    protected string $model = Product::class;
+
+    protected string $title = 'Products';
+
+    public string $column = 'name';
+
+    protected bool $withPolicy = true;
+
+    /**
+     * @return list<class-string<PageContract>>
+     */
+    protected function pages(): array
+    {
+        return [
+            ProductIndexPage::class,
+            ProductFormPage::class,
+            ProductDetailPage::class,
+        ];
+    }
+}
