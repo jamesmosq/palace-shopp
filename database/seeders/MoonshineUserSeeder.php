d4b593b4d960440dc05b41b072a6ee6d812d3892
James Mosquera <Jamesmosqr@gmail.com>
2025-11-25 01:42:57 -0500

    Add MoonShine users seeder

---
+++ database/seeders/MoonshineUserSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class MoonshineUserSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('moonshine_users')->insert([
+            [
+                'name' => 'James',
+                'email' => 'mosquera@gmail.com',
+                'password' => Hash::make('password123'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Coordinador',
+                'email' => 'coordinador@palace.com',
+                'password' => Hash::make('password123'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Vendedor',
+                'email' => 'vendedor@palace.com',
+                'password' => Hash::make('password123'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+    }
+}
