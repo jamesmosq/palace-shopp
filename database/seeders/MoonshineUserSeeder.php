af53e5a4879f625a42942ad2a55c0462f4901dd4
James Mosquera <Jamesmosqr@gmail.com>
2025-11-25 02:20:39 -0500

    Update seeder to use updateOrInsert

--- database/seeders/MoonshineUserSeeder.php
+++ database/seeders/MoonshineUserSeeder.php
@@ -10,31 +10,34 @@ class MoonshineUserSeeder extends Seeder
 {
     public function run()
     {
-        DB::table('moonshine_users')->insert([
+        $users = [
             [
                 'name' => 'James',
                 'email' => 'mosquera@gmail.com',
                 'password' => Hash::make('password123'),
                 'moonshine_user_role_id' => 1,
-                'created_at' => now(),
-                'updated_at' => now(),
             ],
             [
                 'name' => 'Coordinador',
                 'email' => 'coordinador@palace.com',
                 'password' => Hash::make('password123'),
                 'moonshine_user_role_id' => 2,
-                'created_at' => now(),
-                'updated_at' => now(),
             ],
             [
                 'name' => 'Vendedor',
                 'email' => 'vendedor@palace.com',
                 'password' => Hash::make('password123'),
                 'moonshine_user_role_id' => 3,
-                'created_at' => now(),
-                'updated_at' => now(),
             ],
-        ]);
+        ];
+
+        foreach ($users as $user) {
+            DB::table('moonshine_users')->updateOrInsert(
+                ['email' => $user['email']],
+                array_merge($user, [
+                    'updated_at' => now(),
+                ])
+            );
+        }
     }
 }
